00c1c46b0138195f8a23731ec17f2e896398a587
yycholla <hanway.colin@gmail.com>
2024-11-19 12:43:15 -0700

    Save progress

---
+++ main.py
@@ -0,0 +1,21 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while pygame.get_init():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
